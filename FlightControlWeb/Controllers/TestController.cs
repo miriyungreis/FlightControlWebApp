22c7b9051291025d059a93f1165de93fd02fbd6a
miriyungreis <miriyungreis@users.noreply.github.com>
2020-05-08 11:35:54 +0300

    Add project files.

---
+++ FlightControlWeb/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightControlWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok("Hello World API");
+        }
+    }
+}
