0a4ebe979fa4d5d09f2a61bded9cf1ba956711bc
Arye Amsalem <arye.amsalem@gmail.com>
2020-05-10 07:37:54 +0300

    Leaflet Map was loaded !!!!

---
+++ FlightControlWeb/ClientApp/src/components/flight_control/FlightDetails.js
@@ -0,0 +1,14 @@
+﻿import React, { Component } from 'react';
+
+export class FlightDetails extends Component {
+    static displayName = FlightDetails.name;
+
+    render() {
+        return (
+            <div>
+                <h1>Flight Details  Flight Details  Flight Details  Flight Details  Flight Details  Flight Details  Flight Details </h1>
+            </div>
+
+        );
+    }
+}
---
+++ FlightControlWeb/ClientApp/src/components/flight_control/Flights.js
@@ -0,0 +1,22 @@
+﻿import React, { Component } from 'react';
+
+export class Flights extends Component {
+    static displayName = Flights.name;
+
+    render() {
+        return (
+            <div>
+                <h1>My Flights</h1>
+                <h1>External Flights</h1>
+                <h1>External Flights</h1>
+                <h1>External Flights</h1>
+                <h1>External Flights</h1>
+                <h1>External Flights</h1>
+                <h1>External Flights</h1>
+
+
+            </div>
+
+        );
+    }
+}
